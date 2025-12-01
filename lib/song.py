9a5fe43b86fae8d53e777070669303d3b3856884
jamesmutune <tunekivasu01@gmail.com>
2025-12-01 15:43:34 +0300

    complete git commit -mcomplete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,34 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # increment total song count
+        type(self).count += 1
+
+        # add genre to genres list if not already present
+        if genre not in type(self).genres:
+            type(self).genres.append(genre)
+
+        # add artist to artists list if not already present
+        if artist not in type(self).artists:
+            type(self).artists.append(artist)
+
+        # count songs by genre
+        if genre in type(self).genre_count:
+            type(self).genre_count[genre] += 1
+        else:
+            type(self).genre_count[genre] = 1
+
+        # count songs by artist
+        if artist in type(self).artist_count:
+            type(self).artist_count[artist] += 1
+        else:
+            type(self).artist_count[artist] = 1
